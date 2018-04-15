41005afb51a42a9397d363f721909ded587af1cf
dkwazhere <dhealth77@gmail.com>
2018-04-14 21:26:10 -0700

    initial commit

---
+++ clickit-game/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  // Setting this.state.characters to the characterse json array.
+  state = {
+    friends
+  }
+}
+
+export default App;
