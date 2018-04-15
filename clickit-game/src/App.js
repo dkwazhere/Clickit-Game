a22d6819fc9c2116347582c1373cfab486578a45
dkwazhere <dhealth77@gmail.com>
2018-04-15 01:55:40 -0700

    added minor changes, nothing fixed

--- clickit-game/src/App.js
+++ clickit-game/src/App.js
@@ -1,58 +1,19 @@
 import React, { Component } from 'react';
-import llamas from "./llamas.json";
 import './App.css';
+import llamas from "./llamas.json";
 import LlamaCard from "./components/LlamaCard";
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 
 class App extends Component {
-  // Setting this.state.characters to the characterse json array.
-  state = {
-    message: "Click a Llama to begin",
-    topScore: 0,
-    curScore: 0,
-    llama: llamas,
-    unselectedLlamas: llamas
-  };
-    //
-    // componentDidMount() {
-    // }
-    //
-    // shuffleArray = array => {
-    //     for (let i = array.length - 1; i > 0; i--) {
-    //         let j = Math.floor(Math.random() * (i + 1));
-    //         [array[i], array[j]] = [array[j], array[i]];
-    //     }
-    // }
-    //
-    // selectLlama = breed => {
-    //     const findLlama = this.state.unselectedLlamas.find(item => item.breed === breed);
-    //
-    //     if(findLlama === undefined) {
-    //         // failure to select a new llama
-    //         this.setState({
-    //             message: "You guessed incorrectly!",
-    //             topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
-    //             curScore: 0,
-    //             llamas: llamas,
-    //             unselectedLlamas: llamas
-    //         });
-    //     }
-    //     else {
-    //         // success to select a new llama
-    //         const newLlamas = this.state.unselectedLlamas.filter(item => item.breed !== breed);
-    //
-    //         this.setState({
-    //             message: "You guessed correctly!",
-    //             curScore: this.state.curScore + 1,
-    //             llamas: llamas,
-    //             unselectedLlamas: newLlamas
-    //         });
-    //     }
-    //
-    //     this.shuffleArray(llamas);
-    // };
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        llamas: llamas,
+        unselectedLlamas: llamas
+    }
 
     componentDidMount() {
     }
@@ -68,30 +29,29 @@ class App extends Component {
         const findLlama = this.state.unselectedLlamas.find(item => item.breed === breed);
 
         if(findLlama === undefined) {
-            // failure to select a new Llama
+            // failure to select a new llama
             this.setState({
                 message: "You guessed incorrectly!",
                 topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
                 curScore: 0,
                 llamas: llamas,
-                unselectedllamas: llamas
+                unselectedLlamas: llamas
             });
         }
         else {
-            // success to select a new Llama
+            // success to select a new llama
             const newLlamas = this.state.unselectedLlamas.filter(item => item.breed !== breed);
 
             this.setState({
                 message: "You guessed correctly!",
                 curScore: this.state.curScore + 1,
                 llamas: llamas,
-                unselectedLlamas: llamas
+                unselectedLlamas: newLlamas
             });
         }
 
         this.shuffleArray(llamas);
     };
-
 
 render() {
     return (
@@ -103,13 +63,13 @@ render() {
             />
             <Title />
             {
-                this.state.llama.map(llama => (
+                this.state.llamas.map(llamas => (
                     <LlamaCard
-                        id={llama.id}
-                        key={llama.id}
-                        image={llama.image}
-                        name={llama.name}
-                        location={llama.location}
+                        id={llamas.id}
+                        key={llamas.id}
+                        image={llamas.image}
+                        name={llamas.name}
+                        location={llamas.location}
                         selectLlama={this.selectLlama}
                         curScore={this.state.curScore}
                     />
@@ -117,7 +77,7 @@ render() {
             }
         </Wrapper>
     );
-}
+  }
 }
 
 
