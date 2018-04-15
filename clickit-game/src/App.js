18c67d79e96e4bffcfa2c8dc39c3c5446ad491de
dkwazhere <dhealth77@gmail.com>
2018-04-15 00:06:01 -0700

    added style, still working on logic behind game

--- clickit-game/src/App.js
+++ clickit-game/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LlamaCard from "./components/LlamaCard";
+import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 import llamas from "./llamas.json";
@@ -8,30 +9,79 @@ import './App.css';
 class App extends Component {
   // Setting this.state.characters to the characterse json array.
   state = {
-    llamas
+    message: "Click a Llama to begin",
+    topScore: 0,
+    curScore: 0,
+    llama: llamas,
+    unselectedLlamas: llamas
   };
 
-  removeLlama = id => {
-    const llamas = this.state.llamas.filter(llama => llama.id !== id);
-    this.setState({ llamas });
-  };
+    componentDidMount() {
+    }
 
-  render() {
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    selectLlama = breed => {
+        const findLlama = this.state.unselectedLlamas.find(item => item.breed === breed);
+
+        if(findLlama === undefined) {
+            // failure to select a new llama
+            this.setState({
+                message: "You guessed incorrectly!",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                llamas: llamas,
+                unselectedLlamas: llamas
+            });
+        }
+        else {
+            // success to select a new llama
+            const newLlamas = this.state.unselectedLlamas.filter(item => item.breed !== breed);
+
+            this.setState({
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                llamas: llamas,
+                unselectedLlamas: newLlamas
+            });
+        }
+
+        this.shuffleArray(llamas);
+    };
+
+
+render() {
     return (
-      <Wrapper>
-        <Title>Llamma Click</Title>
-        {this.state.llamas.map(llama => (
-          <LlamaCard
-          removeFriend={this.removeFriend}
-          id={llama.id}
-          name={llama.name}
-          image={llama.image}
-          location={llama.location}
-          />
-        ))}
-      </Wrapper>
+        <Wrapper>
+            <Navbar
+                message={this.state.message}
+                curScore={this.state.curScore}
+                topScore={this.state.topScore}
+            />
+            <Title />
+            {
+                this.state.llama.map(llama => (
+                    <LlamaCard
+                        id={llama.id}
+                        key={llama.id}
+                        image={llama.image}
+                        name={llama.name}
+                        location={llama.location}
+                        selectLlama={this.selectLlama}
+                        curScore={this.state.curScore}
+                    />
+                ))
+            }
+        </Wrapper>
     );
-  }
 }
+}
+
+
 
 export default App;
--- clickit-game/src/components/LlamaCard/LlamaCard.js
+++ clickit-game/src/components/LlamaCard/LlamaCard.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./LlamaCard.css";
 
-const LlamaCard = props => (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+  const LlamaCard = props => (
+      <div className="card">
+          <div className="img-container">
+              <a onClick={() => props.selectLlama(props.llamaselect)}
+                  className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+              >
+                  <img alt={props.breed} src={props.image} />
+              </a>
+          </div>
+          <div className="content">
+            <ul>
+              <li>
+                <strong>Name:</strong> {props.name}
+              </li>
+              <li>
+                <strong>Location:</strong> {props.location}
+              </li>
+            </ul>
+          </div>
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeLlama(props.id)} className="remove">
-        𝘅
-      </span>
-    </div>
   );
+
+
 export default LlamaCard;
